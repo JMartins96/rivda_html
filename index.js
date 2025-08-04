eb240aff58ef7f864bd7b893336c3bd82d8fb0e4
JMartins96 <joaorgmartins@gmail.com>
2025-08-04 12:01:25 +0100

    Adicionado formulário base em HTML e com funcionalidades em .js por implementar

---
+++ index.js
@@ -0,0 +1,25 @@
+function onlyOne(checkbox) {
+    const checkboxes = document.getElementById("solicitação");
+    checkboxes.forEach((item) => {
+      if (item !== checkbox) item.checked = false;
+    });
+  }
+
+
+var coll = document.getElementsByClassName("atividade-título");
+var i;
+console.log(coll.length)
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
+
+console.log("Hello")
